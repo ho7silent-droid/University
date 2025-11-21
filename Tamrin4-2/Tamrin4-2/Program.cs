cc78beba268fe7fc5e429797691fc4fa0e4b3797
ho7silent-droid <ho7silent@gmail.com>
2025-11-22 02:07:23 +0330

    Add files via upload

---
+++ Tamrin4-2/Tamrin4-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = new int[5];
+        bool found = false;
+
+        int position = 0;
+
+        Console.WriteLine("Enter 5 Numbers");
+
+        for (int i = 0; i < 5; i++)
+        {
+
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+        int counter = 1;
+        
+        foreach (int x in arr)
+        {
+
+            if (x == 2)
+            {
+                found = true;
+                position = counter;
+                break;
+            }
+            counter++;
+        }
+        if (found == true)
+            Console.WriteLine("The number 2 exist,it was the " + position + "number");
+        else
+            Console.WriteLine("The number 2 does not exist");
+    }
+}
