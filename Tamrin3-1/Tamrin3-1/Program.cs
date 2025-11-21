a1fbaab489111e3466878a7fdd6ba1841c119fcd
ho7silent-droid <ho7silent@gmail.com>
2025-11-22 02:01:51 +0330

    Add files via upload

---
+++ Tamrin3-1/Tamrin3-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number between 1 and 7: ");
+        int n =
+            int.Parse(Console.ReadLine());
+        switch (n)
+        {
+            case 1:
+                Console.WriteLine("Saturday");
+                break;
+            case 2:
+                Console.WriteLine("Sanday");
+                break;
+            case 3:
+                Console.WriteLine("Monday");
+                break;
+            case 4:
+                Console.WriteLine("Tuesday");
+                break;
+            case 5:
+                Console.WriteLine("Wednesday");
+                break;
+            case 6:
+                Console.WriteLine("Thursday");
+                break;
+            case 7:
+                Console.WriteLine("Friday");
+                break;
+            default:
+                Console.WriteLine("End!");
+                break;
+        }
+    }
+}
