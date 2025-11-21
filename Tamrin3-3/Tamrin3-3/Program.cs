f7830350d1563a3599a2636e38702cc340672797
ho7silent-droid <ho7silent@gmail.com>
2025-11-22 02:03:23 +0330

    Add files via upload

---
+++ Tamrin3-3/Tamrin3-3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int number;
+        int result = 1;
+
+        Console.WriteLine("Enter number(for end Enter 0):");
+
+        while (true)
+        {
+            number =
+                int.Parse(Console.ReadLine());
+
+            if (number == 0)
+                break;
+
+            result = result * number;
+        }
+        Console.WriteLine("Multiplication =v" + result);
+        }
+    }
