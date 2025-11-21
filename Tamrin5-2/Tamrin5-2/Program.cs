7d69ffe3c6d724f054a7f1cbdd3dd378b45ec73c
ho7silent-droid <ho7silent@gmail.com>
2025-11-22 02:11:23 +0330

    Add files via upload

---
+++ Tamrin5-2/Tamrin5-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+        static int Multiply(int a, int b)
+    {
+        if (b == 0)
+            return 0;
+        else
+            return a + Multiply(a, b - 1);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter number 1: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter number 2: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int result = Multiply(num1, num2);
+        Console.WriteLine("Product : " + result);
+    }
+}
