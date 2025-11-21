53ae7c53a43db9ada7c481acff4fa2137717fdf5
ho7silent-droid <ho7silent@gmail.com>
2025-11-22 02:04:58 +0330

    Add files via upload

---
+++ Tamrin4-1/Tamrin4-1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class program
+{
+    static void Main()
+    {
+        int[] arr = new int[10];
+        int zoj = 0;
+        int fard = 0;
+
+        Console.WriteLine("Enter 10 numbers");
+
+        for (int i = 0; i < 10; i++)
+        {
+
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+        foreach (int x in arr)
+        {
+            if (x % 2 == 0)
+                zoj++;
+            else
+                fard++;
+        }
+        Console.WriteLine("Number of zoj numbers: " + zoj);
+        Console.WriteLine("Number of fasd numbers: " + fard);
+    }
+}
