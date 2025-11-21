5cc7d8b758edbc4aadcf93200ba65dc99ec31a3f
ho7silent-droid <ho7silent@gmail.com>
2025-11-22 02:09:36 +0330

    Add files via upload

---
+++ Tamrin5-1/Tamrin5-1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+
+    static double Mohit(double a, double b, double c)
+    {
+        return a + b + c;
+    }
+
+    static double Masahat(double a, double b, double c)
+    {
+        double s = Mohit(a, b, c) / 2;
+        return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter side 1: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter side 2: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter side 3: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+      
+        double mohit = Mohit(a, b, c);
+        double masahat = Masahat(a, b, c);
+
+        Console.WriteLine("Mohit triangle : " + mohit);
+        Console.WriteLine("Masahat triangle : " + masahat);
+    }
+}
