19573f5550ba8bee0da0c6d38891725e2232c224
ho7silent-droid <ho7silent@gmail.com>
2025-11-22 02:02:37 +0330

    Add files via upload

---
+++ Tamrin3-2/Tamrin3-2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Console.Write("Enter a letter (E,H,I): ");
+        string Harf = Console.ReadLine();
+    
+    for (int i = 1; i <= 5; i++)
+        {
+            switch (Harf)
+            {
+                case "E":
+                    if (i == 1 || i == 3 || i == 5)
+
+                        Console.WriteLine("*****");
+                    else
+                        Console.WriteLine("*");
+                            break;
+
+                case "H":
+                    if (i == 3)
+                        Console.WriteLine("*****");
+                    else
+                        Console.WriteLine("*   *");
+                    break;
+
+                case "I":
+                    if (i == 1 || i == 5)
+
+                        Console.WriteLine("*****");
+                    else
+                        Console.WriteLine("  *  ");
+                    break;
+
+                default:
+                    Console.WriteLine("Errorrr:) ");
+                    return;
+            }
+        }
+                
+                
+                
+                
+                
+                }
+}
